8a99231e64276bb74237110cea96e068cb49d92c
HVMS <viralsid2330@gmail.com>
2024-06-25 16:04:03 -0300

    refactor: Add NavBarInformation component for displaying information in the navbar

---
+++ components/navbar/navbar_information.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image"
+
+export default function NavBarInformation() {
+    return (
+        <div className="flex flex-col basis-1/2 items-start">
+            <div className="flex mb-4 py-2 px-4 space-x-2 bg-white border-2 border-black rounded-full">
+                <Image
+                    src="/lock.svg"
+                    alt="Lock"
+                    width={24}
+                    height={24}
+                />
+                <span className="font-semibold">We Care About Your Privacy</span>
+            </div>
+            <h2 className="mt-4 text-4xl font-bold">
+                Send Files <span className="text-emerald-600">Securely</span>
+            </h2>
+            <p className="mt-4 text-gray-600">
+                No one can ever access your files that are sent through TransferChain
+                <br /> besides the intended recipients.
+            </p>
+            <div className="flex justify-center mt-8 space-x-14 md:justify-start">
+                <div className="flex flex-col items-center justify-center">
+                    <Image
+                        src="/network.svg"
+                        alt="Network"
+                        width={64}
+                        height={64}
+                    />
+                    <p className="mt-2 text-sm font-semibold">Secure Network</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image
+                        src="/encryption.svg"
+                        alt="Encryption"
+                        width={64}
+                        height={64}
+                    />
+                    <p className="mt-2 text-sm font-semibold">End-to-End Encryption</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image
+                        src="/cloud.svg"
+                        alt="cloud"
+                        width={64}
+                        height={64}
+                    />
+                    <p className="mt-2 text-sm font-semibold">Cloud based storage</p>
+                </div>
+            </div>
+        </div>
+    )
+}
