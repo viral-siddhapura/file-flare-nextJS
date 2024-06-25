23120b85a2cee66079438f9d9a2db66e9fd6a3e6
HVMS <viralsid2330@gmail.com>
2024-06-25 00:29:21 -0300

    refactor: Remove unused code from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import { Navabr } from "@/components/navbar/navbar";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-
 export default function Home() {
   return (
     <main className="flex h-full">
