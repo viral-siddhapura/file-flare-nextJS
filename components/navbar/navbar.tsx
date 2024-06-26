1c605e8be63686ed3e5d35fcad2927a23c61328e
HVMS <viralsid2330@gmail.com>
2024-06-26 01:17:36 -0300

    refactor: Update navbar component import and layout in navbar.tsx

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,18 +3,21 @@ import Image from "next/image"
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 import UploadFile from "../upload/uploadFile"
 import NavBarInformation from "./navbar_information"
+import UploadComplete from "../upload/upload_complete"
 
 export default function NavBar() {
     return (
-        <div className="min-h-screen bg-white">
-            <header className="flex items-center justify-between px-8 py-4 border-b">
+        <div className="flex h-full bg-white">
+            <div className="flex basis-1/2 items-center justify-between px-8 py-4 border-b">
                 <div className="flex items-center space-x-4">
                     <Image src="/logo.svg" alt="File-Flare" width={64} height={64} />
                     <h1 className="text-xl font-bold">
                         The File <span className="text-emerald-600">Flare</span>
                     </h1>
                 </div>
-                <div className="flex items-center space-x-6">
+            </div>
+            <div className="flex basis-1/2 items-center justify-end">
+                <div className="flex basis-1/2 items-center jusitfy-end space-x-6">
                     {/* Put the Menu button in main - navabar page */}
                     <Link href="#" className="text-gray-600 hover:text-gray-900" prefetch={false}>
                         History
@@ -27,11 +30,7 @@ export default function NavBar() {
                         </Avatar>
                     </div>
                 </div>
-            </header>
-            <main className="flex flex-row items-center justify-center py-40">
-                <UploadFile />
-                <NavBarInformation />
-            </main>
+            </div>
         </div>
-    )
+    );
 }
