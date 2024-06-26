ab6ff35244d6b17e2b8e078be784ce7543528cd9
HVMS <viralsid2330@gmail.com>
2024-06-26 01:17:51 -0300

    feat: Add lazy loading for images in uploadFile component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
+import NavBarInformation from "@/components/navbar/navbar_information";
+import UploadFile from "@/components/upload/uploadFile";
+import UploadComplete from "@/components/upload/upload_complete";
+import { useState } from "react";
+
 export default function Home() {
+
+  const [uploadComplete, setUploadComplete] = useState(false);
+
+  const handleUploadComplete = () => {
+    setUploadComplete(true);
+  };
+
   return (
-    <main className="flex h-full">
+    <main className="flex flex-row items-center justify-center py-40">
+      {
+        uploadComplete ? (
+          <UploadComplete />
+        ) : (
+          <UploadFile onUploadComplete={handleUploadComplete} />
+        )
+      }
+      <NavBarInformation />
     </main>
   );
 }
