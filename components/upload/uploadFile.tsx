00f9a45f60e4637086e55de8841b91cd9ad1bf36
HVMS <viralsid2330@gmail.com>
2024-06-26 01:17:44 -0300

    feat: Add lazy loading for images in uploadFile component

--- components/upload/uploadFile.tsx
+++ components/upload/uploadFile.tsx
@@ -1,27 +1,72 @@
+"use client";
+
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
+import UploadInformation from "./upload_information"
+import { useRef, useState, useTransition } from "react"
+import { uploadFileAction } from "@/actions/upload_file";
+import { Input } from "../ui/input";
+import UploadComplete from "./upload_complete";
+import { on } from "events";
 
-const UploadFile = () => {
+const UploadFile = ({ onUploadComplete }: any) => {
+
+    const [file, setFile] = useState<File | null>(null);
+    const [isPending, startTransition] = useTransition();
+    const [error, setError] = useState<string | undefined>("");
+    const [success, setSuccess] = useState<string | undefined>("");
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length > 0) {
+            setFile(e.target.files[0]);
+        }
+    };
+
+    const handleButtonClick = () => {
+
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+
+        if (!file) {
+            console.log("No file selected");
+            return;
+        };
+
+        startTransition(() => {
+            startTransition(async () => {
+                try {
+                    setSuccess("success");
+                    onUploadComplete();
+                } catch (error) {
+                    setError("Error uploading file. Please try again.");
+                    setSuccess("");
+                }
+            });
+        });
+    };
+
     return (
-        <div className="flex flex-row basis-1/2 items-center justify-center">
+        <div className="flex basis-1/2 items-center justify-center">
             <Card className="w-full max-w-md p-8 text-center">
-                <CardHeader>
-                    <div className="flex justify-center pb-8">
-                        <Image
-                            src="/upload.svg"
-                            alt="Upload"
-                            width={64}
-                            height={64}
-                        />
-                    </div>
-                    <CardTitle>Drag and Drop Files Here</CardTitle>
-                    <CardDescription>or click to select files</CardDescription>
-                </CardHeader>
+                <UploadInformation />
                 <CardContent>
-                    <Button variant="default" className="w-full mt-8">
-                        Select File(s)
+                    <Button
+                        variant="default"
+                        className="w-full mt-8"
+                        disabled={isPending}
+                        onClick={handleButtonClick}
+                    >
+                        {isPending ? "Uploading..." : "Select File(s)"}
                     </Button>
+                    <input
+                        type="file"
+                        ref={fileInputRef}
+                        onChange={handleFileChange}
+                        style={{ display: 'none' }}
+                    />
                     <p className="mt-8 text-sm text-gray-500">max. 1 GB and 50 files</p>
                 </CardContent>
             </Card>
