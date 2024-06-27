a06d79be1496e75155a19db4197e52945263df00
HVMS <viralsid2330@gmail.com>
2024-06-27 00:31:49 -0300

    Refactor import statements for UploadFile and UploadInformation components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import NavBarInformation from "@/components/navbar/navbar_information";
+import { FileUploadProvider } from "@/components/upload/file-upload-context";
 import UploadFile from "@/components/upload/upload-file";
 
 export default function Home() {
 
   return (
-    <main className="flex flex-row items-center justify-center py-40">
-      <UploadFile />
-      <NavBarInformation />
-    </main>
+    <FileUploadProvider>
+      <main className="flex flex-row items-center justify-center py-40">
+        <UploadFile />
+        <NavBarInformation />
+      </main>
+    </FileUploadProvider>
   );
 }
