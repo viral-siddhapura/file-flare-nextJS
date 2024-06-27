c01fcade31de01162bd50019c04c29445d2fff97
HVMS <viralsid2330@gmail.com>
2024-06-27 02:27:24 -0300

    feat: Add PasswordProtection component for enabling password protection

---
+++ components/upload/password-protection.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Label } from "../ui/label"
+import { Switch } from "../ui/switch"
+
+const PasswordProtection = () => {
+
+    const [isPasswordProtected, setIsPasswordProtected] = useState(false);
+    
+    return (
+        <div>
+            <div className="border-2 rounded-md border-slate-300 p-2">
+                <div className="flex items-center space-x-2 justify-between">
+                    <Label htmlFor="password-protection">Password protection</Label>
+                    <Switch
+                        id="password-protection"
+                        checked={isPasswordProtected}
+                        onCheckedChange={setIsPasswordProtected}
+                    />
+                </div>
+                <p className="text-sm text-muted-foreground mt-4">
+                    When enabled, the password will be visible to you after your <br /> files are uploaded and the link is
+                    generated.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default PasswordProtection;
