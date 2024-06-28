a2a7f4987e054312fe4d25f4ba10f135576ec44c
HVMS <viralsid2330@gmail.com>
2024-06-28 01:21:53 -0300

    feat: Add email input and message textarea to EmailOption component

--- components/upload/email-option.tsx
+++ components/upload/email-option.tsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
 import { Input } from "../ui/input"
 import { Textarea } from "../ui/textarea"
+import { Badge } from "../ui/badge";
+import { DoorClosedIcon, XIcon } from "lucide-react";
+import { useFileUploadOptions } from "./file-upload-options-context";
 
 const EmailOption = () => {
+
+    const { emails, setEmails } = useFileUploadOptions();
+    const [inputValue, setInputValue] = useState<string>("");
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter" && inputValue !== "") {
+            setEmails([...emails, inputValue]);
+            setInputValue("");
+        }
+    }
+
     return (
         <div className="space-y-2">
-            <Input placeholder="Email to" />
-            <Textarea placeholder="Message (Optional)" className="min-h-[80px]" />
+            <div className="flex flex-wrap gap-2 p-2 border rounded-md">
+                {emails.map((email, index) => (
+                    <Badge key={index} variant="email" className="flex items-center">
+                        {email}
+                        <XIcon
+                            className="w-4 h-4 ml-2 cursor-pointer"
+                            onClick={() => setEmails(emails.filter((_, i) => i !== index))}
+                        />
+                    </Badge>
+                ))}
+                <Input
+                    id="email"
+                    type="email"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    placeholder="Enter email address(s)"
+                    className="flex-1 border"
+                />
+                <Textarea placeholder="Message (Optional)" className="min-h-[80px]" />
+            </div>
         </div>
     )
 }
