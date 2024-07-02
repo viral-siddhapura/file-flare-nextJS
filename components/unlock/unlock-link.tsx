138afa696e5109e12ecc4a55656b38f0607f75ec
HVMS <viralsid2330@gmail.com>
2024-07-02 01:29:08 -0300

    chore: Update UnlockLink component styles and structure

--- components/unlock/unlock-link.tsx
+++ components/unlock/unlock-link.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 
 const UnlockLink = () => {
     return (
-        <div className="flex flex-col items-center justify-center bg-white py-40">
+        <div className="flex flex-col items-center justify-center max-h-screen bg-white">
             <div className="flex flex-col items-center space-y-4">
                 <div className="flex items-center justify-center w-32 h-32 border-2 shadow-md rounded-full">
                     <Image
@@ -17,17 +17,17 @@ const UnlockLink = () => {
                     />
                 </div>
                 <h2 className="text-4xl font-bold text-center">This Link is password protected</h2>
-                <p className="text-center text-sm text-muted-foreground">
+                <p className="text-center text-sm">
                     You need to ask the sender for the password to unlock and access files <span />
-                    <InfoIcon className="inline-block w-4 h-4 text-muted-foreground" />
+                    <InfoIcon className="inline-block w-4 h-4" />
                 </p>
             </div>
-            <div className="p-2">
+            <div className="w-full max-w-lg p-4 space-y-4 mt-10">
                 <div className="relative">
                     <Input type="password" placeholder="Password" className="w-full" />
-                    <EyeOffIcon className="absolute right-3 top-3 w-4 h-4 text-muted-foreground" />
+                    <EyeOffIcon className="absolute right-3 top-3 w-4 h-4" />
                 </div>
-                <Button className="w-full bg-blue-500 text-white mt-4">Unlock</Button>
+                <Button className="w-full default mt-4">Unlock</Button>
             </div>
         </div>
     );
