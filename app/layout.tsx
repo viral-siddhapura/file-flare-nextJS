adcac70fe9e56b3bf3da2a96ded34a605720d695
HVMS <viralsid2330@gmail.com>
2024-07-03 00:55:16 -0300

    Refactor layout component to remove NavBar and update font styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import NavBar from "@/components/navbar/navbar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -23,7 +22,6 @@ export default function RootLayout({
         "min-h-screen bg-background font-sans antialiased",
         inter.variable
       )}>
-        <NavBar />
         {children}</body>
     </html>
   );
