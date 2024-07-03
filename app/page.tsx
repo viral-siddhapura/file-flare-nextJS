38dec30237668a7dc53fbee3483450e019a3c51a
HVMS <viralsid2330@gmail.com>
2024-07-03 00:55:25 -0300

    feat: Add NavBar component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import NavBar from "@/components/navbar/navbar";
 import NavBarInformation from "@/components/navbar/navbar_information";
 import { FileUploadProvider } from "@/components/upload/file-upload-context";
 import UploadFile from "@/components/upload/upload-file";
 
 export default function Home() {
-
   return (
     <FileUploadProvider>
+      <NavBar />
       <main className="flex flex-row items-center justify-between py-40">
         <UploadFile />
         <NavBarInformation />
