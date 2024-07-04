44a2bd3bda30afaca72c17745d44cff6bca460aa
HVMS <viralsid2330@gmail.com>
2024-07-04 15:34:52 -0300

    feat: Add signup form validation and user creation logic

--- actions/signup.ts
+++ actions/signup.ts
@@ -1,7 +1,10 @@
+"use server";
 import { RegisterSchema } from "@/schemas";
 import * as z from 'zod';
 
 import bcrypt from 'bcryptjs';
+import { getUserbyEmail } from "@/data/user";
+import { db } from "@/lib/db";
 
 export const signup = async (values : z.infer<typeof RegisterSchema>) => {
 
@@ -14,6 +17,34 @@ export const signup = async (values : z.infer<typeof RegisterSchema>) => {
     }
 
     const { email, password, confirmPassword } = validateFields.data;
+
     const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedConfirmPassword = await bcrypt.hash(confirmPassword, 10);
+
+    if(password !== confirmPassword){
+        return {
+            error: "Passwords do not match!!",
+        }
+    }
+
+    const existingUser = await getUserbyEmail(email);
+    if(existingUser){
+        return {
+            error: "Email address already exists. Please use another email address!!",
+        }
+    }
+
+    // Creating a user
+    await db.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            confirmPassword: hashedConfirmPassword,
+        },
+    });
+
+    return {
+        success: "User created successfully!!",
+    }
 
 };
