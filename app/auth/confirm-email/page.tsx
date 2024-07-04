e6bdbbbd0775e83a21112cb6e41dcf071ecb5845
HVMS <viralsid2330@gmail.com>
2024-07-04 01:05:36 -0300

    feat: Add ConfirmEmailLayout component for confirming email

---
+++ app/auth/confirm-email/page.tsx
@@ -0,0 +1,9 @@
+import { ConfirmationEmailForm } from "@/components/auth/confirmation-email-form";
+
+const ConfirmEmailVerification = () => {
+    return (
+        <ConfirmationEmailForm />
+    )
+}
+
+export default ConfirmEmailVerification;
