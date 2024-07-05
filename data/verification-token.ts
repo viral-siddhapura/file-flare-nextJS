194665b04bb84989bce6214cee1bedb0e764fe23
HVMS <viralsid2330@gmail.com>
2024-07-05 03:09:20 -0300

    refactor: Add getVerificationTokenByEmail function for retrieving verification token by email

---
+++ data/verification-token.ts
@@ -0,0 +1,16 @@
+import { db } from "@/lib/db";
+
+export const getVerificationTokenByEmail = async (email: string) => {
+    try {
+        const verificationToken = await db.verificationToken.findFirst({
+            where: {
+                email,
+            }
+        });
+
+        return verificationToken;
+    } catch (error) {
+        console.error("Error in getVerificationToken: ", error);
+        return null;
+    }
+}
