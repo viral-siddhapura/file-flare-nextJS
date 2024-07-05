a8298a9944ae2267d98747b765b75d9fb419fabb
HVMS <viralsid2330@gmail.com>
2024-07-05 03:09:31 -0300

    refactor: Add sendVerificationEmail function for sending verification email

---
+++ lib/mail.ts
@@ -0,0 +1,14 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendVerificationEmail = async (email: string, token: string) => {
+    const confirmedLink = `http://localhost:3000/auth/confirm-email?token=${token}`;
+
+    await resend.emails.send({
+        to: email,
+        from: "file-flare@resend.dev",
+        subject: "Confirm your email address",
+        html: `<p>Click the link below to confirm your email address:</p><a href="${confirmedLink}">Confirm your email</a>`,
+    })
+}
