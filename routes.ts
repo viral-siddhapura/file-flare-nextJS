c9451f74acb8daaab61addbd823901a14d59c82b
HVMS <viralsid2330@gmail.com>
2024-07-05 03:09:54 -0300

    refactor: Update routes.ts to include public and auth routes

---
+++ routes.ts
@@ -0,0 +1,16 @@
+export const publicroutes = [
+    "/",
+    "/auth/confirm-email",
+];
+
+export const authroutes = [
+    "/auth/login",
+    "/auth/signup",
+    "/auth/error",
+    "/auth/reset",
+    "/auth/forgot-password",
+];
+
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
