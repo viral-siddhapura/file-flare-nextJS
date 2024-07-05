28bb7ecacae1d351a0a400b0282a8813fdd12705
HVMS <viralsid2330@gmail.com>
2024-07-05 01:34:25 -0300

    refactor: Update FormError component to use red background color for error message

--- components/auth/form-error.tsx
+++ components/auth/form-error.tsx
@@ -11,7 +11,7 @@ export const FormError = ({
     if (!message) return null;
 
     return (
-        <div className="bg-destructive/15 p-3 rounded-sm flex items-center gap-x-2 text-sm text-destructive">
+        <div className="bg-red-200 p-3 rounded-sm flex items-center gap-x-2 text-sm text-destructive">
             <ExclamationTriangleIcon className="h-4 w-4" />
             <p>{message}</p>
         </div>
