5e8846806e3593673075089427414588b58d7176
HVMS <viralsid2330@gmail.com>
2024-07-05 21:33:43 -0300

    refactor: Add generateTwoFactorToken function for generating two-factor authentication token

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { db } from './db';
 import { getVerificationTokenByEmail } from '@/data/verification-token';
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
 
 export const generateVerificationToken = async (email: string) => {
     const token = uuidv4();
@@ -25,3 +26,31 @@ export const generateVerificationToken = async (email: string) => {
 
     return verificationToken;
 } 
+
+export const generateTwoFactorToken = async (email: string) => {
+    // generate 6 digit token
+    const token = Math.floor(100000 + Math.random() * 900000).toString();
+
+    //  Expires after 3 minutes
+    const expires = new Date(new Date().getTime() + 180 * 1000);
+
+    const existingToken = await getTwoFactorTokenByEmail(email);
+    if (existingToken){
+        await db.twoFactorToken.delete({
+            where:{
+                id: existingToken.id
+            }
+        });
+    }
+
+    const twoFactorToken = await db.twoFactorToken.create({
+        data: {
+            email,
+            token,
+            expiresAt: expires,
+        }
+    });
+
+    return twoFactorToken;
+
+}
