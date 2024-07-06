16423612d94856c5046aa81604c885479ca9a0bf
HVMS <viralsid2330@gmail.com>
2024-07-05 21:32:06 -0300

    callbacks, events, handlers for NextAuth set

--- auth.ts
+++ auth.ts
@@ -1,3 +1,62 @@
-import Nextauth from 'next-auth';
+import NextAuth from 'next-auth';
 import authConfig from './auth.config';
+import { UserRole } from '@prisma/client';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { db } from './lib/db';
+import { getUserById } from './data/user';
 
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
+} = NextAuth({
+    pages: {
+        signIn: '/auth/login',
+        error: '/auth/error',
+    },
+    events: {
+        async linkAccount({user}){
+            await db.user.update({
+                where: {
+                    id: user.id,
+                },
+                data:{
+                    emailVerified: new Date(),
+                },
+            });
+        }
+    },
+    callbacks: {
+
+        async session({ token, session }){
+            console.log({
+                sessionToken: token,
+            })
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole;
+            }
+            return session;
+        },
+
+        async jwt({token}){
+            if(!token.sub){
+                return token;
+            }
+
+            const existingUser = await getUserById(token.sub);
+            if(!existingUser){
+                return token;
+            }
+
+            token.role = existingUser.role;
+            return token;
+        }
+    },
+    adapter: PrismaAdapter(db),
+    session: { strategy: 'jwt' },
+    ...authConfig,
+})
