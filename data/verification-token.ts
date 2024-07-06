1c14f9c11b8a8880babc65fffce980fbc347aa48
HVMS <viralsid2330@gmail.com>
2024-07-05 21:33:30 -0300

    refactor: Add getVerficationTokenByToken function for retrieving verification token by token

--- data/verification-token.ts
+++ data/verification-token.ts
@@ -14,3 +14,17 @@ export const getVerificationTokenByEmail = async (email: string) => {
         return null;
     }
 }
+
+export const getVerficationTokenByToken = async (token: string) => {
+    try {
+        const verificationToken = await db.verificationToken.findUnique({
+            where: {
+                token,
+            },
+        });
+        return verificationToken;
+    } catch (error) {
+        console.error("Error in getVerficationToken: ", error);
+        return null;
+    }
+};
