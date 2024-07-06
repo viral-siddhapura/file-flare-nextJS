f1f4e05165ee87cb0a7186d1053b92ed26a07d01
HVMS <viralsid2330@gmail.com>
2024-07-05 21:33:50 -0300

    refactor: Update middleware.ts to include authentication middleware for NextAuth

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,41 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import {
+    DEFAULT_LOGIN_REDIRECT,
+    publicRoutes,
+    authRoutes,
+    apiAuthPrefix,
+} from "./routes";
 
 const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+    const { nextUrl } = req;
+    const loggedIn = !!req.auth;
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRuote = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if (isApiAuthRoute) {
+        return null;
+    }
+
+    if (isAuthRoute) {
+        if (loggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
+        return null;
+    }
+
+    if (!loggedIn && !isPublicRuote) {
+        return Response.redirect(new URL("/auth/login", nextUrl));
+    }
+
+    return null;
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+}
