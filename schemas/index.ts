3dcc980acc5acbf3aee0478a9c6127952f73cd05
HVMS <viralsid2330@gmail.com>
2024-07-08 03:28:58 -0300

    refactor: Update password and reset schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,17 @@
 import * as z from 'zod';
 
+export const NewPasswordSchema = z.object({
+    password: z.string().min(6, {
+        message: "Password should be min 6 characters long",
+    }),
+});
+
+export const ResetSchema = z.object({
+    email: z.string().email({
+        message: "Email is required",
+    }),
+});
+
 export const LoginSchema = z.object({
     email: z.string().email({
         message: "Email is required",
