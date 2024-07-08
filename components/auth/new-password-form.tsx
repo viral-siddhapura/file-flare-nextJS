33cab3a9955846612f64675510667a6e74088653
HVMS <viralsid2330@gmail.com>
2024-07-08 03:29:46 -0300

    refactor: Add new password form component for resetting passwords

---
+++ components/auth/new-password-form.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { CardWrapper } from "./card-wrapper";
+import * as z from "zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { NewPasswordSchema } from "../../schemas";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "../ui/form";
+import { useSearchParams } from "next/navigation";
+import { FormError } from "./form-error";
+import { FormSuccess } from "./form-success";
+import { resetPassword } from "@/actions/reset-password";
+
+export const NewPasswordForm = () => {
+
+    const searchParams = useSearchParams();
+    const token = searchParams.get("token");
+
+    const [error, setError] = useState<string | undefined>("");
+    const [success, setSuccess] = useState<string | undefined>("");
+    const [isPending, startTransition] = useTransition();
+
+    const form = useForm<z.infer<typeof NewPasswordSchema>>({
+        resolver: zodResolver(NewPasswordSchema),
+        defaultValues: {
+            password: "",
+        },
+    });
+
+    const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+        setError("");
+        setSuccess("");
+
+        console.log(values);
+
+        startTransition(() => {
+            resetPassword(values, token)
+                .then((data) => {
+                    setError(data?.error);
+                    //  TODO: Add when we add 2FA support
+                    setSuccess(data?.success);
+                });
+        });
+    };
+
+    return (
+        <main className="flex flex-col items-center w-full py-40">
+            <CardWrapper
+                headerLabel={"Enter a new password"}
+                backButtonLabel={"Back to Login"}
+                backButtonHref={"/auth/login"}
+            >
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-6"
+                    >
+                        <div className="space-y-4">
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                disabled={isPending}
+                                                type="password"
+                                                placeholder="********"
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <FormError message={error} />
+                        <FormSuccess message={success} />
+                        <Button
+                            type="submit"
+                            disabled={isPending}
+                            className="w-full"
+                        >
+                            Reset Password
+                        </Button>
+                    </form>
+                </Form>
+            </CardWrapper>
+        </main>
+    )
+};
