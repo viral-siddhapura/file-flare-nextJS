0b0cbfa84a180f38950efedccb7350d8a34e55ea
HVMS <viralsid2330@gmail.com>
2024-07-08 03:28:03 -0300

    refactor: Update mail.ts to use info@fileflare.cloud as the default sender email address

--- lib/mail.ts
+++ lib/mail.ts
@@ -7,7 +7,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 
     await resend.emails.send({
         to: email,
-        from: "file-flare@resend.dev",
+        from: "info@fileflare.cloud",
         subject: "Confirm your email address",
         html: `<p>Click the link below to confirm your email address:</p><a href="${confirmedLink}">Confirm your email</a>`,
     })
@@ -19,8 +19,22 @@ export const sendTwoFactorEmail = async (
 ) => {
     await resend.emails.send({
         to: email,
-        from: "file-flare@resend.dev",
+        from: "info@fileflare.cloud",
         subject: "Two-factor authentication",
         html: `<p>Your Two Factor Authentication Code:${token}</p>`,
+    });
+}
+
+export const sendPasswordResetEmail = async (
+    email: string,
+    token: string,
+) => {
+    const resetPasswordLink = `http://localhost:3000/auth/reset-password?token=${token}`;
+
+    await resend.emails.send({
+        to: email,
+        from: "info@fileflare.cloud",
+        subject: "Reset your password",
+        html: `<p>Click the link below to reset your password:</p><a href="${resetPasswordLink}">Reset your password</a>`,
     });
 }
