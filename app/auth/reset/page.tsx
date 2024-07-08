f999ef631823e97652a580fafc1fc592cbc8286c
HVMS <viralsid2330@gmail.com>
2024-07-08 03:29:25 -0300

    refactor: Add password reset layout and page components

---
+++ app/auth/reset/page.tsx
@@ -0,0 +1,9 @@
+import { PasswordResetForm } from "@/components/auth/password-reset-form"
+
+const PasswordResetPage = () => {
+    return (
+        <PasswordResetForm />
+    )
+}
+
+export default PasswordResetPage;
