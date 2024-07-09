23b9009ed7976d55d4cc6359dc2106ead5076eff
HVMS <viralsid2330@gmail.com>
2024-07-09 04:32:01 -0300

    refactor: Update confirmation email form to use react-hook-form for OTP validation

--- components/auth/confirmation-email-form.tsx
+++ components/auth/confirmation-email-form.tsx
@@ -1,14 +1,30 @@
 "use client";
 
 import { CardHeader, CardTitle, CardFooter, Card, CardContent } from "../ui/card"
-import { useState, useEffect } from "react"
+import { useState, useEffect, startTransition } from "react"
 import { InputOTP, InputOTPGroup, InputOTPSlot } from "../ui/input-otp"
 import { MailCheckIcon } from "lucide-react"
 import { Button } from "../ui/button"
+import { confirmOtp } from "@/actions/confirm-otp";
+import { useForm } from "react-hook-form";
+import { OtpSchema } from "@/schemas";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { FormError } from "./form-error";
+import { FormSuccess } from "./form-success";
 
-export const ConfirmationEmailForm = () => {
+interface ConfirmationEmailFormProps {
+    email: string;
+}
+
+export const ConfirmationEmailForm = (
+    props: ConfirmationEmailFormProps
+) => {
 
     const [otpValue, setOTPValue] = useState<string>("");
+    const [error, setError] = useState<string | undefined>("");
+    const [success, setSuccess] = useState<string | undefined>("");
     const [countdown, setCountdown] = useState<number>(180);
 
     useEffect(() => {
@@ -34,9 +50,30 @@ export const ConfirmationEmailForm = () => {
         setCountdown(180)
     }
 
-    const onVerifyAccount = () => {
-        console.log("Verifying account...")
-        console.log("OTP value is : ", otpValue);
+    const form = useForm<z.infer<typeof OtpSchema>>({
+        resolver: zodResolver(OtpSchema),
+        defaultValues: {
+            pin: "",
+        },
+    })
+
+    const onSubmit = (values: z.infer<typeof OtpSchema>) => {
+        startTransition(() => {
+            confirmOtp(values.pin, props.email)
+                .then((data) => {
+                    if (data?.error) {
+                        setError(data?.error);
+                    }
+
+                    if (data?.success) {
+                        setSuccess(data?.success);
+                    }
+
+                })
+                .catch((error) => {
+                    setError(error);
+                });
+        });
     }
 
     return (
@@ -46,7 +83,39 @@ export const ConfirmationEmailForm = () => {
                 <CardTitle className="text-2xl font-semibold">Authenticate Your Account</CardTitle>
             </CardHeader>
             <CardContent className="flex flex-col justify-center items-center mt-4">
-                <InputOTP
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                        <FormField
+                            control={form.control}
+                            name="pin"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>One-Time Password</FormLabel>
+                                    <FormControl>
+                                        <InputOTP maxLength={6} {...field}>
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={0} />
+                                                <InputOTPSlot index={1} />
+                                                <InputOTPSlot index={2} />
+                                                <InputOTPSlot index={3} />
+                                                <InputOTPSlot index={4} />
+                                                <InputOTPSlot index={5} />
+                                            </InputOTPGroup>
+                                        </InputOTP>
+                                    </FormControl>
+                                    <FormDescription>
+                                        Please enter the one-time password sent to your email address.
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormError message={error} />
+                        <FormSuccess message={success} />
+                        <Button type="submit">Submit</Button>
+                    </form>
+                </Form>
+                {/* <InputOTP
                     maxLength={6}
                     value={otpValue}
                     onChange={(value) => setOTPValue(value)}
@@ -66,7 +135,7 @@ export const ConfirmationEmailForm = () => {
                     ) : (
                         <p className="mt-4">You entered: {otpValue}</p>
                     )}
-                </div>
+                </div> */}
             </CardContent>
             <CardFooter className="flex flex-col items-center justify-between">
                 {
@@ -93,14 +162,6 @@ export const ConfirmationEmailForm = () => {
                         </div>
                     )
                 }
-                <Button
-                    variant="default"
-                    disabled={otpValue.length !== 6 || countdown === 0}
-                    className="w-full mt-5"
-                    onClick={onVerifyAccount}
-                >
-                    Verify Account
-                </Button>
             </CardFooter>
         </Card>
     )
