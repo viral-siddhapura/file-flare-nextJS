e05345463b94ee26c582df26aa8ce13c9eee9280
HVMS <viralsid2330@gmail.com>
2024-07-08 23:57:44 -0300

    refactor: Update OTP schema to include pin validation

--- schemas/index.ts
+++ schemas/index.ts
@@ -12,6 +12,12 @@ export const ResetSchema = z.object({
     }),
 });
 
+export const OtpSchema = z.object({
+    pin: z.string().min(6, {
+        message: "Your one-time password must be 6 characters.",
+    }),
+});
+
 export const LoginSchema = z.object({
     email: z.string().email({
         message: "Email is required",
@@ -19,7 +25,7 @@ export const LoginSchema = z.object({
     password: z.string().min(1, {
         message: "Password is required",
     }),
-    code: z.optional(z.string()),
+    code: z.string().optional(),
 });
 
 export const RegisterSchema = z.object({
