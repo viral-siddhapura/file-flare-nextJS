94efae01b491e4e9d24f246f75d7227682360aea
HVMS <viralsid2330@gmail.com>
2024-07-08 23:58:29 -0300

    refactor: Add reset password page components and functionality

---
+++ app/auth/reset-password/layout.tsx
@@ -0,0 +1,16 @@
+import LoginNavBar from "@/components/auth/login-nav-bar";
+import { cn } from "@/lib/utils";
+
+export default function NewPasswordLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className={cn(
+            "min-h-screen bg-background font-sans antialiased"
+        )}>
+            <LoginNavBar />
+            {children}</div>
+    );
+}
