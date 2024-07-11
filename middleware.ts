4f32cd4e28ed436feeae4581b03bc024ac5c5d65
HVMS <viralsid2330@gmail.com>
2024-07-11 02:44:44 -0300

    refactor: Update middleware to handle OTP confirmation and redirect after logout

--- middleware.ts
+++ middleware.ts
@@ -5,17 +5,30 @@ import {
     publicRoutes,
     authRoutes,
     apiAuthPrefix,
+    OTP_REDIRECT,
 } from "./routes";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+
     const { nextUrl } = req;
+    console.log("req.auth is : ", req.auth);
     const loggedIn = !!req.auth;
+    console.log("NextURL pathname is : ", nextUrl.pathname);
+    console.log("loggedIn is : ", loggedIn);
+
+    console.log("apiAuthPrefix is : ", apiAuthPrefix);
+    console.log("publicRoutes is : ", publicRoutes);
+    console.log("authRoutes is : ", authRoutes);
+    console.log("DEFAULT_LOGIN_REDIRECT is : ", DEFAULT_LOGIN_REDIRECT);
 
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    console.log("isApiAuthRoute is : ", isApiAuthRoute);
     const isPublicRuote = publicRoutes.includes(nextUrl.pathname);
+    console.log("isPublicRuote is : ", isPublicRuote);
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+    console.log("isAuthRoute is : ", isAuthRoute);
 
     if (isApiAuthRoute) {
         return null;
@@ -28,7 +41,14 @@ export default auth((req) => {
         return null;
     }
 
-    if (!loggedIn && !isPublicRuote) {
+    // if logout then redirect to "/" through checking loggedIn
+    if(loggedIn && nextUrl.pathname === "/auth/confirm-otp"){
+        console.log("logged in");
+        return Response.redirect(new URL("/", nextUrl));
+    }
+
+    if (!loggedIn && !isPublicRuote && !isSignUpRoute) {
+        console.log("not logged in");
         return Response.redirect(new URL("/auth/login", nextUrl));
     }
 
