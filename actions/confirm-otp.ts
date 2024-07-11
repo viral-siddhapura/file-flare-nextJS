e15b7fdb4fd77237cda8ee9f0ea8d11a2e26dd4b
HVMS <viralsid2330@gmail.com>
2024-07-11 02:42:35 -0300

    refactor: Improve confirmOtp action for OTP validation

--- actions/confirm-otp.ts
+++ actions/confirm-otp.ts
@@ -1,12 +1,15 @@
 "use server";
 
+import { getTwoFactorConfirmationById } from "@/data/two-factor-confirmation";
 import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 import { getUserByEmail } from "@/data/user";
 import { db } from "@/lib/db";
+import { generateTwoFactorToken } from "@/lib/tokens";
+import { sendTwoFactorEmail } from "@/lib/mail";
 
-export const confirmOtp = async (pin: string, email: string) => {
+export const confirmOtp = async (code: string, email: string, password: string) => {
 
-    console.log("pin is : ", pin);
+    console.log("pin is : ", code);
     console.log("email is : ", email);
 
     const existingUser = await getUserByEmail(email);
@@ -18,18 +21,56 @@ export const confirmOtp = async (pin: string, email: string) => {
         };
     }
 
-    // get the twofactor token from the db
-    const twoFactorToken = await getTwoFactorTokenByEmail(email);
-    console.log("twoFactorToken is : ", twoFactorToken);
+    if (existingUser.isTwoFactorAuthEnabled && existingUser.email) {
+        if (code !=null && code !== "") {
 
-    // Now match the pin entered by the user and twoFactorToken
-    if (twoFactorToken?.token !== pin) {
-        return {
-            error: "Invalid pin",
-        };
+            // TODO: Verify two factor code
+            const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
+            if (!twoFactorToken) {
+                return { error: "Invalid two factor token!" };
+            }
+            if (twoFactorToken.token !== code) {
+                return { error: "Invalid two factor token!" };
+            }
+
+            const hasExpired = new Date() > new Date(twoFactorToken.expiresAt);
+            if (hasExpired) {
+                return { error: "Two factor token has expired!" };
+            }
+
+            await db.twoFactorToken.delete({
+                where: {
+                    id: twoFactorToken.id,
+                },
+            });
+
+            const existingConfirmation = await getTwoFactorConfirmationById(twoFactorToken.id);
+            if (existingConfirmation) {
+                await db.twoFactorConfirmation.delete({
+                    where: {
+                        id: existingConfirmation.id,
+                    },
+                });
+            }
+
+            await db.twoFactorConfirmation.create({
+                data: {
+                    userId: existingUser.id,
+                },
+            });
+
+            return { success: "OTP verified!" };
+
+        } else {
+            console.log("code is not generated yet, so send email immediately");
+            const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+            await sendTwoFactorEmail(existingUser.email, twoFactorToken.token);
+            return { success: "OTP sent!" };
+        }
+
+    } else {
+        console.log("bad things can happen!!");
+        console.log(existingUser.isTwoFactorAuthEnabled);
+        console.log(existingUser.email);
     }
-
-    return {
-        success: "OTP verified successfully",
-    };
 }
