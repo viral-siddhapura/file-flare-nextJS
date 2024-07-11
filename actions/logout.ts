af08722946be33e0494a8e75b339191347941128
HVMS <viralsid2330@gmail.com>
2024-07-11 02:42:57 -0300

    refactor: Add redirect after logout in logout action

--- actions/logout.ts
+++ actions/logout.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { signOut } from "@/auth";
 
 export const logout = async () => {
     // Server side log out
     await signOut();
+    redirect("/");
 };
