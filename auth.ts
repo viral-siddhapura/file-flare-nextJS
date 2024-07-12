284107073c23e4dd8fb7387784c4ca21b74501db
HVMS <viralsid2330@gmail.com>
2024-07-12 02:16:47 -0300

    refactor: back to original code from NextJS -Auth Module

--- auth.ts
+++ auth.ts
@@ -5,7 +5,6 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from './lib/db';
 import { getUserById } from './data/user';
 import { getTwoFactorConfirmationById } from './data/two-factor-confirmation';
-import { exit } from 'process';
 
 export const {
     handlers: {GET, POST},
@@ -49,27 +48,22 @@ export const {
                 return false;
             }
 
-            console.log("existing user two factor is true or false : ",exisitingUser.isTwoFactorAuthEnabled);
-            console.log("existing user email is : ",exisitingUser.email);
-            console.log("existing user email verified is : ",exisitingUser.emailVerified);
-            console.log("existing user is id is : ",exisitingUser.id);
-
             // TODO: Add 2FA check here
-            // if (exisitingUser.isTwoFactorAuthEnabled){
+            if (exisitingUser.isTwoFactorAuthEnabled){
                 
-            //     const twoFactorConfirmation = await getTwoFactorConfirmationById(exisitingUser.id);
-            //     console.log(twoFactorConfirmation);
-            //     if (!twoFactorConfirmation){
-            //         return false;
-            //     }
+                const twoFactorConfirmation = await getTwoFactorConfirmationById(exisitingUser.id);
+                console.log(twoFactorConfirmation);
+                if (!twoFactorConfirmation){
+                    return false;
+                }
 
-            //     // Delete two factor confirmation for next sign in
-            //     await db.twoFactorConfirmation.delete({
-            //         where: {
-            //             id: twoFactorConfirmation.id,
-            //         },
-            //     });
-            // }
+                // Delete two factor confirmation for next sign in
+                await db.twoFactorConfirmation.delete({
+                    where: {
+                        id: twoFactorConfirmation.id,
+                    },
+                });
+            }
 
             return true;
 
