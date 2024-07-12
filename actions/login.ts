1cb98c5ba8832ae3455417cad4ee9f62182ec222
HVMS <viralsid2330@gmail.com>
2024-07-12 02:14:15 -0300

    refactor: login action to manage two factor and login both actions in the same server actions

--- actions/login.ts
+++ actions/login.ts
@@ -7,22 +7,25 @@ import { sendVerificationEmail, sendTwoFactorEmail } from "@/lib/mail";
 import { getUserByEmail } from "@/data/user";
 import bcrypt from "bcryptjs";
 import { signIn } from "@/auth";
-import { OTP_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
+import { getTwoFactorConfirmationById } from "@/data/two-factor-confirmation";
+import { db } from "@/lib/db";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
 
     const validatedFields = LoginSchema.safeParse(values);
-    console.log("validated fields are : ",validatedFields);
+    console.log("validated fields are : ", validatedFields);
 
-    if(!validatedFields.success){
+    if (!validatedFields.success) {
         return {
             error: "Invalid fields",
         };
     }
 
     const { email, password, code } = validatedFields.data;
-    console.log("email, password, code are : ",email, password, code);
+    console.log("email, password, code are : ", email, password, code);
 
     const existingUser = await getUserByEmail(email);
     if (!existingUser || !existingUser.email || !existingUser.password) {
@@ -31,38 +34,75 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         };
     }
 
-    console.log("existingUser is : ",existingUser);
+    console.log("existingUser is : ", existingUser);
 
     // prevent login if the passwords are not matched? - throw error
-    const passwordMatch = await bcrypt.compare(
-        password, 
-        existingUser.password
-    );
+    // const passwordMatch = await bcrypt.compare(
+    //     password,
+    //     existingUser.password
+    // );
 
-    if(!passwordMatch){
-        return {
-            error: "Invalid password",
-        };
-    }
+    // if (!passwordMatch) {
+    //     return {
+    //         error: "Invalid password",
+    //     };
+    // }
 
     // prevent login if email is not verified, send an email for verfication again
-    if(!existingUser.emailVerified){
+    if (!existingUser.emailVerified) {
         const verificationToken = await generateVerificationToken(existingUser.email);
         await sendVerificationEmail(verificationToken.email, verificationToken.token);
         return { success: "Please verify your email address. Confirmation Email Sent!" };
     }
 
-    if(existingUser.isTwoFactorAuthEnabled && existingUser.email) {
-        console.log("code is not generated yet, so send email immediately");
-        const twoFactorToken = await generateTwoFactorToken(existingUser.email);
-        await sendTwoFactorEmail(existingUser.email, twoFactorToken.token);
+    if (existingUser.isTwoFactorAuthEnabled && existingUser.email) {
+        if (code) {
+            // TODO: Verify two factor code
+            const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
+            if (!twoFactorToken){
+                return { error: "Invalid two factor token!" };
+            }
+            if (twoFactorToken.token !== code) {                
+                return { error: "Invalid two factor token!" };
+            } 
+
+            const hasExpired = new Date() > new Date(twoFactorToken.expiresAt);
+            if (hasExpired) {
+                return { error: "Two factor token has expired!" };
+            }
+
+            await db.twoFactorToken.delete({
+                where: {
+                    id: twoFactorToken.id,
+                },
+            });
+
+            const exisingConfirmation = await getTwoFactorConfirmationById(twoFactorToken.id);
+            if (exisingConfirmation) {
+                await db.twoFactorConfirmation.delete({
+                    where: {
+                        id: exisingConfirmation.id,
+                    },
+                });
+            }
+
+            await db.twoFactorConfirmation.create({
+                data: {
+                    userId: existingUser.id,
+                },
+            });
+
+        } else {
+            const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+            await sendTwoFactorEmail(existingUser.email, twoFactorToken.token);
+            return { twoFactor: true };
+        }
 
         try {
-            console.log("now we are redirecting to OTP_REDIRECT after signIn via credential part: ",OTP_REDIRECT);
             await signIn("credentials", {
                 email,
                 password,
-                redirectTo: OTP_REDIRECT,
+                redirectTo: DEFAULT_LOGIN_REDIRECT,
             });
         } catch (error) {
             if (error instanceof AuthError) {
