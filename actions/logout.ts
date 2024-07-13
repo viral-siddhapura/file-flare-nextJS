6258904546a6c0115f2838afb77643c51dca3023
HVMS <viralsid2330@gmail.com>
2024-07-12 22:11:54 -0300

    logout sever action refactor: commented out console logs

--- actions/logout.ts
+++ actions/logout.ts
@@ -1,10 +1,8 @@
 "use server";
 
-import { redirect } from "next/navigation";
 import { signOut } from "@/auth";
 
 export const logout = async () => {
     // Server side log out
     await signOut();
-    redirect("/");
 };
