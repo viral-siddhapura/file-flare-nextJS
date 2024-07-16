56337b2313f9b6df8b4e9186a21d1459a6205a5b
HVMS <viralsid2330@gmail.com>
2024-07-15 23:43:44 -0300

    fix: responsive ness added in main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import NavBar from "@/components/navbar/navbar";
 import NavBarInformation from "@/components/navbar/navbar_information";
 import { FileUploadProvider } from "@/components/upload/file-upload-context";
 import UploadFile from "@/components/upload/upload-file";
-import Landing from "./landing/page";
 
 export default function Home() {
   return (
     <FileUploadProvider>
+      {/* <Landing /> */}
       <NavBar />
-      <main className="flex flex-row items-center justify-center py-40">
+      <main className="w-full min-h-screen flex flex-col md:flex-row items-center justify-center p-4">
         <UploadFile />
         <NavBarInformation />
       </main>
