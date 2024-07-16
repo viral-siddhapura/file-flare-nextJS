e907c90921292aef5e11350810853d307a7f7f13
HVMS <viralsid2330@gmail.com>
2024-07-15 23:45:08 -0300

    fix: responsiveness added in upload information file component

--- components/upload/upload-information.tsx
+++ components/upload/upload-information.tsx
@@ -4,16 +4,17 @@ import Image from "next/image"
 export default function UploadInformation() {
     return (
         <CardHeader>
-            <div className="flex justify-center pb-8">
+            <div className="flex justify-center pb-4 md:pb-8">
                 <Image
                     src="/upload.svg"
                     alt="Upload"
                     width={64}
                     height={64}
+                    className="w-12 h-12 md:w-16 md:h-16"
                 />
             </div>
-            <CardTitle>Drag and Drop Files Here</CardTitle>
-            <CardDescription>or click to select files</CardDescription>
+            <CardTitle className="text-xl md:text-2xl mb-2">Drag and Drop Files Here</CardTitle>
+            <CardDescription className="text-sm md:text-base">or click to select files</CardDescription>
         </CardHeader>
     )
 }
