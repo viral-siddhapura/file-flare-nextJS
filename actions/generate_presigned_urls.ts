d60299ae4e8bfbb28755803db20b1d1f511a79bc
HVMS <viralsid2330@gmail.com>
2024-07-17 03:55:31 -0300

    generate presigned post urls for uploading files through lambda functions

---
+++ actions/generate_presigned_urls.ts
@@ -0,0 +1,20 @@
+export const generatePresignedUrls = async (files: File[]) => {
+    
+    console.log("files", files);
+
+    const fileDetails = Array.from(files).map(file => ({
+        fileName: file.name,
+        fileType: file.type,
+    }));
+
+    try {
+        const preSignedUrls = await fetch(`https://x5rtytm247jx4zaapzhlzkg4740phtpe.lambda-url.us-east-1.on.aws/`,{
+            method: 'POST',
+            body: JSON.stringify({ files: fileDetails })
+        });
+
+        return preSignedUrls;
+    } catch (error) {   
+        console.log("error", error);
+    }
+}
