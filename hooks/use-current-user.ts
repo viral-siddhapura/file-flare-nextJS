f08beccb91eb9fd7c99c1a8aa37a09202bd14788
HVMS <viralsid2330@gmail.com>
2024-07-24 04:52:15 -0300

    getSession() method used

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -1,7 +1,7 @@
-import { useSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
-export const useCurrentUser = () => {
-    const session = useSession();
+export const useCurrentUser = async () => {
+    const session = await getSession();
 
-    return session.data?.user;
+    return session?.user;
 };
