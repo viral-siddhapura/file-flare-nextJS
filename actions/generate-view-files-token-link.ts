47a54f6d7fe625f378c0c2d8dcee2d4973a0e2a9
HVMS <viralsid2330@gmail.com>
2024-07-26 01:05:15 -0300

    fix: regenerate token for the uploaded files

--- actions/generate-view-files-token-link.ts
+++ actions/generate-view-files-token-link.ts
@@ -2,27 +2,41 @@
 
 import { db } from "@/lib/db";
 
-export const generateViewFilesTokenLink = async (numberOfExpiryDays: number, isPasswordProtected: boolean, password: string) => {
-    const randomToken = Math.random().toString(36).substring(2, 15) + "-" + Math.random().toString(36).substring(2, 15);
+export const generateViewFilesTokenLink = async (token: string, numberOfExpiryDays: number, isPasswordProtected: boolean, password: string) => {
 
-    // this randomToken must be expired after numberOfExpiryDays
-    const tokenExpiresAt = new Date(new Date().getTime() + numberOfExpiryDays * 24 * 60 * 60 * 1000);
-    console.log("tokenExpiresAt is : ", tokenExpiresAt);
+    try {
+        const existingToken = await db.viewFilesPasswordToken.findUnique({
+            where: {
+                token: token,
+            },
+        });
 
-    // now save this randomToken and tokenExpiresAt in database
-    const response = await db.viewFilesPasswordToken.create({
-        data: {
-            token: randomToken,
-            expiresAt: tokenExpiresAt,
-            password: password
+        if (existingToken) {
+            // Update existing token
+            const updatedToken = await db.viewFilesPasswordToken.update({
+                where: { token: token },
+                data: {
+                    expiresAt: new Date(Date.now() + numberOfExpiryDays * 24 * 60 * 60 * 1000),
+                    password: password,
+                },
+            });
+            const existingLink = `http://localhost:3000/viewfiles/${updatedToken.token}`
+            return existingLink;
+        } else {
+            // Create new token
+            const newToken = await db.viewFilesPasswordToken.create({
+                data: {
+                    token: token,
+                    expiresAt: new Date(Date.now() + numberOfExpiryDays * 24 * 60 * 60 * 1000),
+                    password: password,
+                },
+            });
+
+            const newLink = `http://localhost:3000/viewfiles/${newToken.token}`;
+            return newLink;
         }
-    });
-
-    if (!response) {
-        throw new Error("Failed to generate token link");
-    }else{
-        const link = `http://localhost:3000/viewfiles/${randomToken}`;
-        console.log("link is : ", link);
-        return link;
+    } catch (error) {
+        console.error("Error generating token link:", error);
+        return null;
     }
 }
