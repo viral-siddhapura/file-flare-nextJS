bbf2f6052c44a38ee5882213cc85fe07f28c4d36
HVMS <viralsid2330@gmail.com>
2024-07-26 01:11:58 -0300

    speed insights enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { auth } from "@/auth";
 import { SessionProvider } from "next-auth/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,7 @@ export default async function RootLayout({
   return (
     <SessionProvider session={session}>
       <html lang="en" suppressHydrationWarning>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>{children}<SpeedInsights /></body>
       </html>
     </SessionProvider>
   );
