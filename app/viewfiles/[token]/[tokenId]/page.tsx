ea9aae5096cfbdf9945ed259df3c5ec65195f32e
HVMS <viralsid2330@gmail.com>
2024-08-05 23:55:24 -0300

    server action code added and token converted into fullToken

--- app/viewfiles/[token]/[tokenId]/page.tsx
+++ app/viewfiles/[token]/[tokenId]/page.tsx
@@ -4,28 +4,44 @@ import Image from "next/image";
 import { InfoIcon, EyeOffIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { checkViewFilesTokenExpired } from "@/actions/check-view-files-token-expired";
+import { verifyTokenPassword } from "@/actions/verify-token-password";
 
-export default function TokenPage({ params }: { params: { token: string } }) {
+export default function TokenPage({ params }: { params: { token: string, tokenId: string } }) {
 
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const router = useRouter();
+  
     const [isTokenExpired, setIsTokenExpired] = useState<boolean>(false);
+
+    const fullToken = `${params.token}/${params.tokenId}`;
 
     useEffect(() => {
         const checkToken = async () => {
-            const token = params.token;
-            const isTokenExpired = await checkViewFilesTokenExpired(token);
+            const isTokenExpired = await checkViewFilesTokenExpired(fullToken);
             console.log("isTokenExpired is : ", isTokenExpired);
 
             setIsTokenExpired(isTokenExpired);
         };
         checkToken();
-    }, [params.token]);
+    }, [fullToken]);
 
-    console.log("TokenPage rendered with token:", params.token);
+    console.log("TokenPage rendered with token:", fullToken);
     const pathname = usePathname();
     console.log("pathname is : ", pathname);
+
+    const handleUnlock = async () => {
+        const result = await verifyTokenPassword(fullToken, password);
+        if (result.success) {
+          console.log(result.message);
+          router.push(`/`);
+        } else {
+          setError(result.message);
+        }
+      };
 
     return (
         <>
@@ -49,10 +65,15 @@ export default function TokenPage({ params }: { params: { token: string } }) {
                         </div>
                         <div className="w-full max-w-lg p-4 mt-10">
                             <div className="relative">
-                                <Input type="password" placeholder="Password" className="w-full" />
+                                <Input 
+                                type="password" 
+                                placeholder="Password" 
+                                className="w-full"
+                                onChange={(e) => setPassword(e.target.value)} />
                                 <EyeOffIcon className="absolute right-3 top-3 w-4 h-4" />
                             </div>
-                            <Button className="w-full default mt-4">Unlock</Button>
+                            {error && <p className="text-red-500 mt-2">{error}</p>}
+                            <Button className="w-full default mt-4" onClick={handleUnlock}>Unlock</Button>
                         </div>
                     </div>
                 )
