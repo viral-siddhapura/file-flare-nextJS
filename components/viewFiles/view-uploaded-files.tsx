f902c7d770f3b85dbc2502c3ccc3366d73ffb866
HVMS <viralsid2330@gmail.com>
2024-08-06 22:39:18 -0300

    rendered conditional components for sending emails vs generating links

--- components/viewFiles/view-uploaded-files.tsx
+++ components/viewFiles/view-uploaded-files.tsx
@@ -5,12 +5,8 @@ import { useFileUploadOptions } from "../upload/file-upload-options-context";
 import { useContext, useRef, useState } from "react";
 import { FileUploadContext } from "../upload/file-upload-context";
 import { generateExpirationUrls } from "@/actions/generate_expiration_urls";
-import { useToast } from "../ui/use-toast";
-import { Toast, ToastDescription, ToastProvider, ToastTitle } from "../ui/toast";
-import { db } from "@/lib/db";
 import { generateViewFilesTokenLink } from "@/actions/generate-view-files-token-link";
-import { set } from "zod";
-import { link } from "fs";
+import Image from "next/image";
 
 const ViewUploadedFiles = () => {
 
@@ -86,42 +82,63 @@ const ViewUploadedFiles = () => {
     }
 
     return (
-        <div className="flex flex-col items-center justify-center p-6 bg-white border-2 border-green-500 rounded-full w-[470px] h-[470px] mx-auto">
-            <div className="flex items-center justify-center w-20 h-20 bg-gradient-to-r from-green-500 to-emerald-500 rounded-full">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-12 h-12 text-white"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                >
-                    <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm5 7.59l-5.59 5.59a1 1 0 0 1-1.42 0L7 12.42a1 1 0 0 1 1.42-1.42L11 13.17l4.59-4.59a1 1 0 0 1 1.42 1.42z" />
-                </svg>
-            </div>
-            <h2 className="mt-4 text-xl font-bold text-center text-gray-900">Link is created securely</h2>
-            <p className="mt-2 text-center text-gray-500">
-                Make sure to copy the password to access the files.
-                <InfoIcon className="inline-block w-4 h-4 ml-1 text-gray-400" />
-            </p>
-            <div className="relative flex items-center justify-center mt-4">
-                <Input type="text" value="2EEF7D" readOnly ref={passwordRef} className="w-40 p-2 text-center border rounded" />
-                <Button variant="ghost" className="absolute right-0 p-2" onClick={copyPassword}>
-                    <CopyIcon className="w-5 h-5 text-gray-500" />
-                </Button>
-            </div>
-            <div className="flex flex-col justify-center mt-6">
-                <Button variant="ghost" onClick={generateLink}>
-                    <div className="flex flex-col items-center">
-                        <LinkIcon className="w-5 h-5 text-blue-500" />
-                        <span className="text-sm text-black">Copy Link</span>
+        <div className="flex flex-col items-center justify-center p-6 bg-white border-4 border-green-500 rounded-full w-[470px] h-[470px] mx-auto">
+            {
+                emails.length > 0 ? (
+                    <div className="flex flex-col items-center justify-center p-6">
+                        <Image
+                            src="/success_clouds.svg"
+                            alt="Email Icon"
+                            width={64}
+                            height={64}
+                        />
+                        <h2 className="mb-2 text-lg font-semibold text-center text-gray-800">Files Sent Securely</h2>
+                        <p className="mb-6 text-center text-gray-600">Your transfer will be available for {expiryDate}s.</p>
+                        <Button variant="ghost" className="px-6 py-2 text-white bg-gradient-to-r from-blue-400 to-blue-600 rounded-md">
+                            View Details
+                        </Button>
                     </div>
-                </Button>
-                {linkCopied && (
-                    <div className="mt-2 flex items-center justify-center space-x-2 text-green-500">
-                        <CircleCheckIcon className="w-5 h-5" />
-                        <span>Link copied</span>
-                    </div>
-                )}
-            </div>
+                ) :
+                    (
+                        <>
+                            <div className="flex items-center justify-center w-20 h-20 bg-gradient-to-r from-green-500 to-emerald-500 rounded-full">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-12 h-12 text-white"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                >
+                                    <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm5 7.59l-5.59 5.59a1 1 0 0 1-1.42 0L7 12.42a1 1 0 0 1 1.42-1.42L11 13.17l4.59-4.59a1 1 0 0 1 1.42 1.42z" />
+                                </svg>
+                            </div>
+                            <h2 className="mt-4 text-xl font-bold text-center text-gray-900">Link is created securely</h2>
+                            <p className="mt-2 text-center text-gray-500">
+                                Make sure to copy the password to access the files.
+                                <InfoIcon className="inline-block w-4 h-4 ml-1 text-gray-400" />
+                            </p>
+                            <div className="relative flex items-center justify-center mt-4">
+                                <Input type="text" value="2EEF7D" readOnly ref={passwordRef} className="w-40 p-2 text-center border rounded" />
+                                <Button variant="ghost" className="absolute right-0 p-2" onClick={copyPassword}>
+                                    <CopyIcon className="w-5 h-5 text-gray-500" />
+                                </Button>
+                            </div>
+                            <div className="flex flex-col justify-center mt-6">
+                                <Button variant="ghost" onClick={generateLink}>
+                                    <div className="flex flex-col items-center">
+                                        <LinkIcon className="w-5 h-5 text-blue-500" />
+                                        <span className="text-sm text-black">Copy Link</span>
+                                    </div>
+                                </Button>
+                                {linkCopied && (
+                                    <div className="mt-2 flex items-center justify-center space-x-2 text-green-500">
+                                        <CircleCheckIcon className="w-5 h-5" />
+                                        <span>Link copied</span>
+                                    </div>
+                                )}
+                            </div>
+                        </>
+                    )
+            }
         </div>
     )
 }
